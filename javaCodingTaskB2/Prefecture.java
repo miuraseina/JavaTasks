13511387521fa96cf25a7537c1ba6305ff2e30d0
miuraseina <s.miura@miracleave.co.jp>
2026-04-28 14:53:14 +0900

    first commit

---
+++ javaCodingTaskB2/Prefecture.java
@@ -0,0 +1,37 @@
+package javaCodingTaskB2;
+
+
+
+public class Prefecture {
+
+
+	private int id;
+	private String name;
+	private int capitalCityId;
+
+	public Prefecture(int id, String name, int capitalCityId) {
+		this.id = id;
+		this.name = name;
+		this.capitalCityId = capitalCityId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getCapitalCityId() {
+		return capitalCityId;
+	}
+
+
+
+
+}
+
+
+
+	
