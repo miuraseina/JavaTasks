f4e16d3f9bb424c7b1ff2ddde2712e83c8146eb7
miuraseina <s.miura@miracleave.co.jp>
2026-04-30 16:30:53 +0900

    first commit

---
+++ javaCodingTaskB3/Main.java
@@ -0,0 +1,31 @@
+package javaCodingTaskB3;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("給与基準額を入力してください");
+		try (Scanner sc = new Scanner(System.in)) {
+			int payrollStandardAmount = sc.nextInt();
+
+
+			CsvReader reader = new CsvReader();
+			List<Employee> employees = reader.readCsv();
+
+			EmployeeService service = new EmployeeService();
+
+			List<Employee> result = service.salaryComparison(employees, payrollStandardAmount);
+
+			for (Employee emp : result) {
+				System.out.println(emp);
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
